e7716fdf69edd3fbe2d13bd8fc28135191436890
Daniwisssss <dana.contreras2002@gmail.com>
2021-10-20 23:31:37 -0500

    service

---
+++ src/main/java/pe/edu/upc/spring/service/IRequestService.java
@@ -0,0 +1,20 @@
+
+package pe.edu.upc.spring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+import pe.edu.upc.spring.model.Request;
+
+public interface IRequestService {
+
+	public boolean insertar (Request request);
+	public boolean modificar(Request request);
+	public void eliminar(int idRequest);
+	public Optional<Request> listarId(int idRequest);
+	List<Request> listar();
+	List<Request> buscarNamePlanner(String name);
+	List<Request> buscarNameEvent(String nameEvent);
+	
+}
